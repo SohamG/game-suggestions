920ce54ab408ff977013d07cc195a557641d7d3b
Soham Gumaste <sohamg2@gmail.com>
2017-10-10 14:18:46 +0530

    Added card macro. Not implemented yet.

--- app/main.py
+++ app/main.py
@@ -3,15 +3,24 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
+app.jinja_env.auto_reload = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['MONGO_DBNAME'] = 'db1'
 app.config['MONGO_URI'] = 'mongodb://root:root123@ds113915.mlab.com:13915/db1'
 
 mongo = PyMongo(app)
 
+
 @app.route("/")
 def hello():
-    # suggestions = 
+    
     return render_template("main.html")
+
+
+@app.route("/card")
+def cards():
+    return render_template("card.html")
+
 
 if __name__ == "__main__":
     # Only for debugging while developing
