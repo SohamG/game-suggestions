b196f622df326b39df00da7acf19bdae67777703
Soham Gumaste <sohamg2@gmail.com>
2017-10-11 14:02:05 +0530

    Started implementing cards visually TODO: db stuff

--- app/main.py
+++ app/main.py
@@ -13,8 +13,30 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def hello():
-    
-    return render_template("main.html")
+    listings1 = (
+        {
+            'title': "Wrangler shield is too OP",
+            'tags': ['#tf2', '#engineer'],
+            'info': 'We all know wrangler has blah blah blah s s s s s s s s s ss s s s',
+            'up': 100, 
+            'down': 10
+        },
+        {
+            'title': "Wrangler shield is too OP",
+            'tags': ['#tf2', '#engineer'],
+            'info': 'We all know wrangler has blah blah blah s s s s s s s s s ss s s s',
+            'up': 100, 
+            'down': 10
+        },
+        {
+            'title': "Wrangler shield is too OP",
+            'tags': ['#tf2', '#engineer'],
+            'info': 'We all know wrangler has blah blah blah s s s s s s s s s ss s s s',
+            'up': 100, 
+            'down': 10
+        }
+    )
+    return render_template("main.html", listings1=listings1)
 
 
 @app.route("/card")
