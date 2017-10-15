dbd1e7ec6335a4e70596d9ad038531867f67db68
Soham Gumaste <sohamg2@gmail.com>
2017-10-15 13:28:06 +0530

    Removed sql bs

--- app/main.py
+++ app/main.py
@@ -1,35 +1,17 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.jinja_env.auto_reload = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://user1:passwd123@localhost/gamesuggest'
 
-db = SQLAlchemy(app)
-
-# Schemas
-
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    poster = db.Column(db.String(80), unique=True, nullable=False)
-    post_title = db.Column
-    up = db.Column(db.Integer)
-    down = db.Column(db.Integer)
-
-    
-
-    def __repr__(self):
-        return '<User %r>' % self.
 
 
 # Routes
 @app.route("/")
 def hello():
     #listings1 =  
-    return render_template("main.html", listings1=listings1)
+    return render_template("main.html", listings1={'title': 'WIP'})
 
 
 @app.route("/card")
