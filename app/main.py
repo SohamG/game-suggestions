0becf2675ab4eca78c81d2cf4c62c36781664401
Soham Gumaste <sohamg2@gmail.com>
2017-10-22 14:23:17 +0530

    Back from being sick. TODO: Commit before leaving

--- app/main.py
+++ app/main.py
@@ -1,17 +1,31 @@
 from flask import Flask, render_template
+import redis
 
 app = Flask(__name__)
 
 app.jinja_env.auto_reload = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
+r = redis.Redis(host='172.17.0.2', port=6379)
+
+if r is None:
+    print("Redis aint workin yo")
+
+
+'''
+Format for Posts hash -  
+
+Poster(str)    Title(str)   Tags(list(str))     Info(str)   Up(int)     Down(int)  
+
+'''
 
 
 # Routes
 @app.route("/")
 def hello():
     #listings1 =  
-    return render_template("main.html", listings1={'title': 'WIP'})
+    # return render_template("main.html", listings1={'title': 'WIP'})
+    return render_template('landing.html')
 
 
 @app.route("/card")
